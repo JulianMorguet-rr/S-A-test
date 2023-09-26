b9a5336282fce19eb9b93f6ca4f0f760fdbe18fe
Julian Morguet <julian@MBP-von-test.fritz.box>
2023-09-26 14:42:18 +0200

    Added astro astro-robots-txt integration

--- Astro/astro.config.mjs
+++ Astro/astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import sanity from "astro-sanity";
-
 import vue from "@astrojs/vue";
-
 import node from "@astrojs/node";
-
 import tailwind from "@astrojs/tailwind";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,13 +14,12 @@ export default defineConfig({
     dataset: "production",
     apiVersion: "v2021-03-25",
     useCdn: true
-  }), vue(), tailwind()],
-  // images: {
-  //   domains: ['cdn.sanity.io', 'geschmaecker-sind-verschieden.com/'],
-  //   remotePatterns: [{protocol: 'https'}]
-  // },
+  }), vue(), tailwind(), robotsTxt()],
+  images: {
+    domains: ['cdn.sanity.io', 'geschmaecker-sind-verschieden.com/'],
+    remotePatterns: [{protocol: 'https'}]
+  },
   adapter: node({
     mode: "standalone"
-  }),
-  
+  })
 });
