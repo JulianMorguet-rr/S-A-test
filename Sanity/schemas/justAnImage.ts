c7ced7cfe1be303fec60301d84a41e52d435dc5c
Julian Morguet <julian@MacBook-Pro-von-test.local>
2024-01-23 13:19:38 +0100

    update Sanity and add more functions to "sticky Text with Image" and "just an Image"

--- Sanity/schemas/justAnImage.ts
+++ Sanity/schemas/justAnImage.ts
@@ -15,6 +15,66 @@ export const justAnImage = defineType({
       type: 'string',
     }),
     defineField({
+      name: 'centerText',
+      title: 'Center Text?',
+      type: 'boolean',
+      initialValue: false,
+    }),
+    defineField({
+      name: 'useCustomColorGradient',
+      title: 'Use custom color gradient',
+      type: 'boolean',
+      initialValue: false,
+    }),
+    defineField({
+      name: 'customColorGradient',
+      title: 'Custom color gradient for Heading',
+      type: 'object',
+      hidden: ({parent, value}) => parent?.useCustomColorGradiend !== true,
+      fields: [
+        defineField({
+          name: 'color1',
+          title: 'Color 1',
+          type: 'string',
+          initialValue: '#000000',
+          validation: Rule => Rule.custom((color1) => {
+            // write a function that checks if the color is valid
+            if(typeof color1 !== 'string') return 'Please enter a valid color'
+            if (color1.charAt(0) !== '#') return 'Please enter a valid color'
+            const hex = color1.slice(1);
+            if (hex.length !== 3 && hex.length !== 6) return 'Please enter a valid color'
+            for (let i = 0; i < hex.length; i++) {
+              const char = hex.charAt(i).toLowerCase();
+              if (!(/[0-9a-f]/.test(char))) {
+                return 'Please enter a valid color'
+              }
+            }
+            return true
+          })
+        }),
+        defineField({
+          name: 'color2',
+          title: 'Color 2',
+          type: 'string',
+          initialValue: '#000000',
+          validation: Rule => Rule.custom((color1) => {
+            // write a function that checks if the color is valid
+            if(typeof color1 !== 'string') return 'Please enter a valid color'
+            if (color1.charAt(0) !== '#') return 'Please enter a valid color'
+            const hex = color1.slice(1);
+            if (hex.length !== 3 && hex.length !== 6) return 'Please enter a valid color'
+            for (let i = 0; i < hex.length; i++) {
+              const char = hex.charAt(i).toLowerCase();
+              if (!(/[0-9a-f]/.test(char))) {
+                return 'Please enter a valid color'
+              }
+            }
+            return true
+          })
+        }),
+      ],
+    }),
+    defineField({
       name: 'image',
       type: 'image',
       options: {hotspot: true},
--- Sanity/schemas/textWidthStickyImage.tsx
+++ Sanity/schemas/textWidthStickyImage.tsx
@@ -41,12 +41,66 @@ export const textWithStickyImage = defineType({
       type: 'string',
     }),
     defineField({
+      name: 'tagline',
+      type: 'string',
+    }),
+    defineField({
       name: 'heading',
       type: 'string',
     }),
     defineField({
-      name: 'tagline',
-      type: 'string',
+      name: 'useCustomColorGradiend',
+      title: 'Use custom color gradient for Heading',
+      type: 'boolean',
+      initialValue: false,
+    }),
+    defineField({
+      name: 'customColorGradient',
+      title: 'Custom color gradient',
+      type: 'object',
+      hidden: ({parent, value}) => parent?.useCustomColorGradiend !== true,
+      fields: [
+        defineField({
+          name: 'color1',
+          title: 'Color 1',
+          type: 'string',
+          initialValue: '#000000',
+          validation: Rule => Rule.custom((color1) => {
+            // write a function that checks if the color is valid
+            if(typeof color1 !== 'string') return 'Please enter a valid color'
+            if (color1.charAt(0) !== '#') return 'Please enter a valid color'
+            const hex = color1.slice(1);
+            if (hex.length !== 3 && hex.length !== 6) return 'Please enter a valid color'
+            for (let i = 0; i < hex.length; i++) {
+              const char = hex.charAt(i).toLowerCase();
+              if (!(/[0-9a-f]/.test(char))) {
+                return 'Please enter a valid color'
+              }
+            }
+            return true
+          })
+        }),
+        defineField({
+          name: 'color2',
+          title: 'Color 2',
+          type: 'string',
+          initialValue: '#000000',
+          validation: Rule => Rule.custom((color1) => {
+            // write a function that checks if the color is valid
+            if(typeof color1 !== 'string') return 'Please enter a valid color'
+            if (color1.charAt(0) !== '#') return 'Please enter a valid color'
+            const hex = color1.slice(1);
+            if (hex.length !== 3 && hex.length !== 6) return 'Please enter a valid color'
+            for (let i = 0; i < hex.length; i++) {
+              const char = hex.charAt(i).toLowerCase();
+              if (!(/[0-9a-f]/.test(char))) {
+                return 'Please enter a valid color'
+              }
+            }
+            return true
+          })
+        }),
+      ],
     }),
     defineField({
       name: 'blockContent',
